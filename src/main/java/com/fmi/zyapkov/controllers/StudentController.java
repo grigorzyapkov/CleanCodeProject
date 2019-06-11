f20ed436d18f999434940d5f92efbb64b19066c4
Grigor Zyapkov <46219158+grigorzyapkov@users.noreply.github.com>
2019-06-11 11:44:21 +0300

    Fix javadocs for controllers (#4)

--- src/main/java/com/fmi/zyapkov/controllers/StudentController.java
+++ src/main/java/com/fmi/zyapkov/controllers/StudentController.java
@@ -16,36 +16,65 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    /**
+     * @param id
+     * @return student by id
+     */
     @GetMapping(value= "/{id}" , produces = "application/json")
     public Student getById(@PathVariable Long id){
         return studentService.getById(id);
     }
 
+    /**
+     * @return list of students
+     */
     @GetMapping(produces = "application/json")
     public List<Student> getAll(){
         return studentService.getAll();
     }
 
+    /**
+     * @param student
+     * @return create new student
+     */
     @PostMapping
     public Student createStudent(@RequestBody @Valid Student student){
         return studentService.create(student);
     }
 
+    /**
+     * This endpoint increment student's grade with one
+     * @param id
+     * @return updated student
+     */
     @PostMapping("/{id}/increment-grade")
     public Student incrementStudentGradeByOne(@PathVariable Long id){
        return studentService.incrementStudentGradeByOne(id);
     }
 
+    /**
+     * This method add new completed course for student with specific id
+     * @param id
+     * @param course
+     * @return updated student
+     */
     @PostMapping("/{id}/add-course")
     public Student addCompletedCourse(@PathVariable Long id, @RequestBody Course course){
         return studentService.addCompletedCourse(id, course);
     }
 
+    /**
+     * This method delete student with specific id
+     * @param id
+     */
     @DeleteMapping(value = "/{id}")
     public void deleteById(@PathVariable Long id){
         studentService.deleteById(id);
     }
 
+    /**
+     * This method delete all students in database
+     */
     @DeleteMapping
     public void deleteAll(){
         studentService.deleteAll();
--- src/main/java/com/fmi/zyapkov/controllers/UniversityController.java
+++ src/main/java/com/fmi/zyapkov/controllers/UniversityController.java
@@ -15,26 +15,44 @@ public class UniversityController {
     @Autowired
     private UniversityService universityService;
 
+    /**
+     * @param id
+     * @return university with specific id
+     */
     @GetMapping(value= "/{id}" , produces = "application/json")
     public University getById(@PathVariable Long id){
         return universityService.getById(id);
     }
 
+    /**
+     * @return list of all universities
+     */
     @GetMapping(produces = "application/json")
     public List<University> getAll(){
         return universityService.getAll();
     }
 
+    /**
+     * @param university
+     * @return university that was created
+     */
     @PostMapping
     public University createUniversity(@RequestBody @Valid University university){
         return universityService.create(university);
     }
 
+    /**
+     * This method delete university with specific id
+     * @param id
+     */
     @DeleteMapping(value = "/{id}")
     public void deleteById(@PathVariable Long id){
         universityService.deleteById(id);
     }
 
+    /**
+     * This method delete all universities in database
+     */
     @DeleteMapping
     public void deleteAll(){
         universityService.deleteAll();
