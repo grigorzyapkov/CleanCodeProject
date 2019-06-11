9a530041b00cc154118e32a781969e53f04f142c
ziapkovv <grigor.ziapkovv@gmail.com>
2019-06-11 11:43:32 +0300

    Fix javadocs for controllers

--- src/main/java/com/fmi/zyapkov/controllers/LecturerController.java
+++ src/main/java/com/fmi/zyapkov/controllers/LecturerController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ *
+ */
 @RestController
 @RequestMapping("lecturers")
 public class LecturerController {
@@ -15,26 +18,46 @@ public class LecturerController {
     @Autowired
     private LecturerService lecturerService;
 
+    /**
+     * @param id
+     * @return lecturer by specific id
+     */
     @GetMapping(value= "/{id}" , produces = "application/json")
     public Lecturer getById(@PathVariable Long id){
         return lecturerService.getById(id);
     }
 
+
+    /**
+     * @return list of lecturers
+     */
     @GetMapping(produces = "application/json")
     public List<Lecturer> getAll(){
         return lecturerService.getAll();
     }
 
+    /**
+     * @param lecturer
+     * @return create new lecturer
+     */
     @PostMapping
     public Lecturer createLecturer(@RequestBody @Valid Lecturer lecturer){
         return lecturerService.create(lecturer);
     }
 
+    /**
+     * @param id
+     *
+     * This method delete lecturer by id if such exists
+     */
     @DeleteMapping(value = "/{id}")
     public void deleteById(@PathVariable Long id){
         lecturerService.deleteById(id);
     }
 
+    /**
+     * This method delete all lecturers in database
+     */
     @DeleteMapping
     public void deleteAll(){
         lecturerService.deleteAll();
